41f84572603549205a3b4f60a61f28a6f4fba7e4
kawsar firoz <kawsarfiroz11@gmail.com>
2025-04-16 18:19:08 +0300

    added callback hell example

--- callbackHell.js
+++ callbackHell.js
@@ -1,20 +1,61 @@
 
 
 
+// example 1 ======================
+const func1=()=>{
+    console.log(" function1-------------first"); 
+    setTimeout(() => {
+        console.log(" function1-------------second")
+        setTimeout(() => {
+            console.log(" function1-------------third")
+            setTimeout(() => {
+                console.log(" function1-------------4th")
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}
+setTimeout(() => {
+    func1()
+}, 4000);
 
-// const getRoll= ()=>{
-//    setTimeout(() => {
-//     console.log("getting roll no");
-//     let roll_No=[1,2,3,4];
-//     console.log(roll_No);
-//     setTimeout((roll) => {
-//         const bioData= {name:"kawsar",age:25, }
-//         console.log(bioData)
-//     }, 2000,roll_No[1]);
-//    }, 2000);
-// } 
 
-// console.log(getRoll());
+//exampl2 using promise ==================
+const func2=(msg, time)=>{
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            console.log(msg)
+        }, time);
+        resolve();
+    })
+};
 
+setTimeout(() => {
+    func2("func_2 ============== first", 1000)
+    .then(()=>func2("func_2 ============== three",500))
+    .then(()=>func2("func_2 ============== four",500))
+    .then(()=>func2("func2============== fourth",300))
+}, 4000);
+
+
+// example 3 with async await =====================
+const func3=(msg, time)=>{
+
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            console.log(msg) ;
+            resolve()
+        }, time);
+    })
+}
+const runAll= async()=>{
+    await func3("function3________first",1000)
+    await func3("function3________second", 3000)
+    await func3("function3________third", 3000)
+
+}
+
+setTimeout(() => {
+    runAll()
+}, 6000);
 
 
